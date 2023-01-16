520d6f8c6afd24c461752679da4a51695a59615c
cobaltune <cobaltune@naver.com>
2023-01-16 20:13:43 +0900

    feat: Add `id`, Change `propTypes`

--- horror/src/components/Movie.js
+++ horror/src/components/Movie.js
@@ -1,12 +1,12 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-function Movie({ mediumCoverImage, title, year, summary, genres }) {
+function Movie({ id, mediumCoverImage, title, year, summary, genres }) {
   return (
     <div>
       <img src={mediumCoverImage} alt={title}></img>
       <h2>
-        <Link to='/movie'>{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
       <h3>({year})</h3>
       <p>{summary}</p>
@@ -20,9 +20,10 @@ function Movie({ mediumCoverImage, title, year, summary, genres }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   mediumCoverImage: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  year: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
