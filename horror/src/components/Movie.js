a5a9d801bfedd95a9e74f14899c3ce7e44a70486
cobaltune <cobaltune@naver.com>
2023-01-16 18:03:07 +0900

    feat: Add `Link`

--- horror/src/components/Movie.js
+++ horror/src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function Movie({ mediumCoverImage, title, year, summary, genres }) {
   return (
     <div>
       <img src={mediumCoverImage} alt={title}></img>
-      <h2>{title}</h2>
+      <h2>
+        <Link to='/movie'>{title}</Link>
+      </h2>
       <h3>({year})</h3>
       <p>{summary}</p>
       <ul>
